9693de5a4709784b72e9a0838415897579fed983
Hipoha <reda.mateusz@gmail.com>
2018-02-16 19:30:40 +0100

    Email sending: code refactoring
    19.3. [theory, part 2]

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+    private String mailTo;
+    private String subject;
+    private String message;
+}
