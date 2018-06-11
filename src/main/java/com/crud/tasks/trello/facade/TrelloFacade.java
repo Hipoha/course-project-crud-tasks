13170c6ec5e65e26007df4733fed5effee9ec45a
Meteo-R <reda.mateusz@gmail.com>
2018-06-11 22:52:18 +0200

    Filtering boards containing string "test"
    21.1. REST API 3: Facade [theory 3]

--- src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
+++ src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
@@ -4,11 +4,15 @@ import com.crud.tasks.domain.TrelloBoard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.mapper.TrelloMapper;
 import com.crud.tasks.service.TrelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TrelloFacade {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrelloFacade.class);
 
     @Autowired
     private TrelloService trelloService;
@@ -16,8 +20,14 @@ public class TrelloFacade {
     @Autowired
     private TrelloMapper trelloMapper;
 
-    public List<TrelloBoard> fetchTrelloBoards() {
-        return trelloMapper.mapToBoards(trelloService.fetchTrelloBoards());
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        List<TrelloBoard> trelloBoards = trelloMapper.mapToBoards(trelloService.fetchTrelloBoards());
+        LOGGER.info("Filtering boards...");
+        List<TrelloBoard> filteredBoards = trelloBoards.stream()
+                .filter(trelloBoard -> !trelloBoard.getName().equalsIgnoreCase("test"))
+                .collect(Collectors.toList());
+        LOGGER.info("Boards have been filtered. Current list size: " + filteredBoards.size());
+        return trelloMapper.mapToBoardsDto(filteredBoards);
     }
 
 }
